d6f6ef69957b079f6b7ce27139e62565d40f1072
hidemikawai <yu93_min@yahoo.co.jp>
2018-07-10 22:14:43 +0900

    PinBallプロジェクト　初めてのコミット
    Lesson5に課題～発展課題を追記したファイルです。

---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	private int totalscore = 0;
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.scoreText.GetComponent<Text> ().text = "Score: " + this.totalscore;
+	}
+
+	void OnCollisionEnter (Collision other){
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.totalscore += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.totalscore += 20;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.totalscore += 50;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.totalscore += 100;
+		}
+	}
+	}
